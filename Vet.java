01478e82f050cebef6dc8d24c924565fd1a52ca9
FelipeASP2 <72319157+FelipeASP2@users.noreply.github.com>
2022-08-31 21:08:51 -0300

    Update Vet.java

--- Vet.java
+++ Vet.java
@@ -2,28 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.mavenproject1.newpackage;
+package com.mycompany.clinicavet;
 
 /**
  *
  * @author f169401
  */
 public class Vet {
-    
-    private int ID;
     private String nome;
-    private String endereco;
+    private String email;
     private String telefone;
 
-    public Vet(int ID, String nome, String endereco, String telefone) {
-        this.ID = ID;
+    public Vet(String nome, String email, String telefone) {
         this.nome = nome;
-        this.endereco = endereco;
+        this.email = email;
         this.telefone = telefone;
-    }
-
-    public int getID() {
-        return ID;
     }
 
     public String getNome() {
@@ -34,12 +27,12 @@ public class Vet {
         this.nome = nome;
     }
 
-    public String getEndereco() {
-        return endereco;
+    public String getEmail() {
+        return email;
     }
 
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getTelefone() {
@@ -49,7 +42,6 @@ public class Vet {
     public void setTelefone(String telefone) {
         this.telefone = telefone;
     }
-    
-    
+ 
     
 }
