7d808719e99260a1f43d19244890aa531ed85e68
FelipeASP2 <72319157+FelipeASP2@users.noreply.github.com>
2022-08-31 21:02:16 -0300

    Update Consulta.java

--- Consulta.java
+++ Consulta.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.mavenproject1.newpackage;
+package com.mycompany.clinicavet;
 
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -11,36 +12,80 @@ import java.util.Date;
  * @author f169401
  */
 public class Consulta {
+    private Calendar data;
+    private int horas;
+    private String comentarios;
+    private int idAnimal;
+    private int idVet;
+    private int idTratamento;
+    private boolean terminou;
+
+    public Consulta(Calendar data, int horas, String comentarios, int idAnimal, int idVet, int idTratamento, boolean terminou) {
+        this.data = data;
+        this.horas = horas;
+        this.comentarios = comentarios;
+        this.idAnimal = idAnimal;
+        this.idVet = idVet;
+        this.idTratamento = idTratamento;
+        this.terminou = terminou;
+    }
+
+    public Calendar getData() {
+        return data;
+    }
+
+    public void setData(Calendar data) {
+        this.data = data;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public String getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(String comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    public int getIdAnimal() {
+        return idAnimal;
+    }
+
+    public void setIdAnimal(int idAnimal) {
+        this.idAnimal = idAnimal;
+    }
+
+    public int getIdVet() {
+        return idVet;
+    }
+
+    public void setIdVet(int idVet) {
+        this.idVet = idVet;
+    }
+
+    public int getIdTratamento() {
+        return idTratamento;
+    }
+
+    public void setIdTratamento(int idTratamento) {
+        this.idTratamento = idTratamento;
+    }
+
+    public boolean isTerminou() {
+        return terminou;
+    }
+
+    public void setTerminou(boolean terminou) {
+        this.terminou = terminou;
+    }
     
-    private Date dataconsulta;
-    private String historico;
-    private int ID;
-
-    public Consulta(Date dataconsulta, String historico, int ID) {
-        this.dataconsulta = dataconsulta;
-        this.historico = historico;
-        this.ID = ID;
-    }
-
-    public Date getDataconsulta() {
-        return dataconsulta;
-    }
-
-    public void setDataconsulta(Date dataconsulta) {
-        this.dataconsulta = dataconsulta;
-    }
-
-    public String getHistorico() {
-        return historico;
-    }
-
-    public void setHistorico(String historico) {
-        this.historico = historico;
-    }
-
-    public int getID() {
-        return ID;
-    }
     
-    
+ 
 }
