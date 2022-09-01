f53729325288fcd35ec3f0b9fa2e9415a7fa1876
FelipeASP2 <72319157+FelipeASP2@users.noreply.github.com>
2022-08-31 21:02:52 -0300

    Update Espécie.java

--- Espécie.java
+++ Espécie.java
@@ -2,34 +2,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.mavenproject1.newpackage;
+package com.mycompany.clinicavet;
 
 /**
  *
  * @author f169401
  */
 public class Espécie {
-    
-    private int ID;
     private String nome;
-
-    public Espécie(int ID, String nome) {
-        this.ID = ID;
+    
+    public Espécie(String nome) {
         this.nome = nome;
     }
-
-    public int getID() {
-        return ID;
-    }
-
+    
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    
-    
-    
 }
